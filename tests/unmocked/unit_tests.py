efc17e62da7833465354fbcac392976fa87112c6
EC2 Default User <ec2-user@ip-172-31-80-137.ec2.internal>
2021-03-16 01:07:17 +0000

    Unit Test no mocking completed

--- tests/unmocked/unit_tests.py
+++ tests/unmocked/unit_tests.py
@@ -3,47 +3,16 @@ import os
 import sys
 
 sys.path.append(os.path.abspath('../../'))
+from app import add_user
 from app import log_chat
+from app import remove_user
 import user_template
 
 KEY_INPUT = "input"
 KEY_USERNAME = "user"
 KEY_EXPECTED = "expected"
 
-class SendChatTestCase(unittest.TestCase):
-    def setUp(self):
-        self.success_test_params = [
-            {
-                KEY_INPUT: 'Test Sentence',
-                KEY_USERNAME: "user",
-                KEY_EXPECTED: '<user>: Test Sentence',
-            },
-            {
-                KEY_INPUT: 'Test-/+*0\'Sentence',
-                KEY_USERNAME: "james",
-                KEY_EXPECTED: '<user>: Test-/+*0\'Sentence',
-            },
-            {
-                KEY_INPUT: '',
-                KEY_USERNAME: "sfes_asdf",
-                KEY_EXPECTED: '<user>: ',
-            },
-            {
-                KEY_INPUT: '\n\n\n',
-                KEY_USERNAME: "tictac",
-                KEY_EXPECTED: '<user>: \n\n\n',
-            },
-        ]
-        
-    def test_username_success(self):
-        for test in self.success_test_params:
-            data = {'username': KEY_USERNAME, 'message': test[KEY_INPUT]}
-            actual_result = log_chat(data)
-            
-            expected_result = test[KEY_EXPECTED]
-            
-            self.assertEqual(len(actual_result), len(expected_result))
-            self.assertEqual(actual_result, expected_result)
+INITIAL_USERLIST = ['user', 'james', 'sfes_asdf', 'tictac']
 
 class SendChatTestCase(unittest.TestCase):
     def setUp(self):
@@ -56,24 +25,55 @@ class SendChatTestCase(unittest.TestCase):
             {
                 KEY_INPUT: 'Test-/+*0\'Sentence',
                 KEY_USERNAME: "james",
-                KEY_EXPECTED: '<user>: Test-/+*0\'Sentence',
+                KEY_EXPECTED: '<james>: Test-/+*0\'Sentence',
             },
             {
                 KEY_INPUT: '',
                 KEY_USERNAME: "sfes_asdf",
-                KEY_EXPECTED: '<user>: ',
+                KEY_EXPECTED: '<sfes_asdf>: ',
             },
             {
                 KEY_INPUT: '\n\n\n',
                 KEY_USERNAME: "tictac",
-                KEY_EXPECTED: '<user>: \n\n\n',
+                KEY_EXPECTED: '<tictac>: \n\n\n',
             },
         ]
         
-    def test_username_success(self):
+    def test_chat_success(self):
         for test in self.success_test_params:
-            data = {'username': KEY_USERNAME, 'message': test[KEY_INPUT]}
-            actual_result = log_chat(data)
+            actual_result = log_chat({'username': test[KEY_USERNAME], 'message': test[KEY_INPUT]})
+            
+            expected_result = test[KEY_EXPECTED]
+            
+            self.assertEqual(len(actual_result), len(expected_result))
+            self.assertEqual(actual_result, expected_result)
+
+class RemoveAnyOneUserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_USERNAME: "user",
+                KEY_EXPECTED: ['james', 'sfes_asdf', 'tictac'],
+            },
+            {
+                KEY_USERNAME: "james",
+                KEY_EXPECTED: ['user', 'sfes_asdf', 'tictac'],
+            },
+            {
+                KEY_USERNAME: "sfes_asdf",
+                KEY_EXPECTED: ['user', 'james', 'tictac'],
+            },
+            {
+                KEY_USERNAME: "tictac",
+                KEY_EXPECTED: ['user', 'james', 'sfes_asdf'],
+            },
+        ]
+        
+    def test_remove_success(self):
+        for person in INITIAL_USERLIST:
+            add_user(person)
+        for test in self.success_test_params:
+            actual_result = remove_user(test[KEY_USERNAME])
             
             expected_result = test[KEY_EXPECTED]
             
